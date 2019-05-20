67bd9671dfed4c815af94d99bca7c7a6b171e14c
Gabriel Pietroluongo <gabrielpietroluongo@gmail.com>
2019-05-20 18:59:16 -0300

    WIP: Linearização

--- scripts/questao4.m
+++ scripts/questao4.m
@@ -8,12 +8,24 @@ load "../dados/ajuste_bacteria.txt";
 s = ajuste_bacteria(1:l, 1);
 v = ajuste_bacteria(1:l, 2);
 
+
+# Linearizados
 x = arrayfun(@(p) 1./p, s);
 z = arrayfun(@(p) 1./p, v);
 
-pol = polyfit(x, z, 1)
+# Polinômio linearizado
+lin = polyfit(x, z, 1);
+polyout(lin)
+
+# Espaço amostral: [0,1]
+xi = linspace(0, 1);
+
+# Plot
+linAprox = polyval(lin, xi);
+plot (x, z, "*", xi, linAprox, ";Aproximação Linear;")
 
 # 1/Vm = 0.19022 -> Vm = 1/0.19022 -> Vm = 5.2571
 # Ks/Vm = 16.40224 -> Ks = 3.1200
 
-# p(s) = (5.2571*3.1200)/(3.1200 + s)
+# p(s) = (5.2571*S)/(3.1200 + s)
+
