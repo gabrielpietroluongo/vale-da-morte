8d30fbddcc398d0eaacd23b3da719fa30a1841cf
Gabriel Pietroluongo <gabrielpietroluongo@gmail.com>
2019-05-20 00:55:11 +0000

    WIP: Solução da Primeira questão de ajuste

--- scripts/questao3.m
+++ scripts/questao3.m
@@ -25,3 +25,18 @@ printf("pol|    1     |    2     |    3     |    4     |    5     |\n")
 printf("r² | %f | %f | %f | %f | %f |\n", rsq1, rsq2, rsq3, rsq4, rsq5);
 printf("var| %f | %f | %f | %f | %f |\n", vari1, vari2, vari3, vari4, vari5);
 disp ("\n\nAnalisando a tabela, vemos que a menor variância ocorre quando o grau do polinômio é 3. Assim, usaremos esse polinômio para aproximar a função.");
+
+disp(' ')
+
+input (s_Enter_Continue);
+
+clc
+fprintf('p3(x) = %3.2f x³  %3.2f x² + %3.2f x  %3.2f\n', p3(1), p3(2), p3(3), p3(4));
+xi = linspace(2.6,3.1);
+z3 = polyval(p3, xi);
+
+plot(x, y, "*", xi, z3, "; Polinômio aproximante de Grau 3;")
+
+fprintf("\n\nSeguindo p3(x), temos p(2.8) = %f\n\n",  (33.01*(2.8)^3)-(259.86*(2.8)^2) + 675.15*2.8 - 571.83);
+
+input(s_Enter_Continue);
