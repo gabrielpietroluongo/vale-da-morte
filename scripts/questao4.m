641009c0e0a0387510827dfaaa984e76d34d3223
Gabriel Pietroluongo <gabrielpietroluongo@gmail.com>
2019-05-24 04:26:06 +0000

    Apresentação da questão 4 melhorada

--- scripts/questao4.m
+++ scripts/questao4.m
@@ -1,20 +1,34 @@
 clc
-clear -except strings
+clear -x bShouldUseCache
+strings_all
+strings_q4
 
 load (strcat(data_path, s_Adjust_Bacteria_FName));
 [l, c] = size(ajuste_bacteria);
+
 # Linearizando como 1/v = Ks/Vm * 1/s + 1/Vm
 # temos a = Ks/Vm, x = 1/s, b = 1/Vm
 s = ajuste_bacteria(1:l, 1);
 v = ajuste_bacteria(1:l, 2);
 
+disp(s_Linearization_First_Exp);
+disp("\n")
+
+input(s_Enter_Continue);
+clc;
+
 # Linearizados
 x = 1./s;
-z = 1./v
+z = 1./v;
 
 # Polinômio linearizado
 lin = polyfit(x, z, 1);
-polyout(lin)
+disp(s_Linearization_Poly);
+polLin = polyout(lin);
+printf("P(s) = %s", polLin)
+disp("\n");
+input(s_Enter_Continue);
+clc;
 
 # Espaço amostral (linearizado): [0,1]
 xi = linspace(0, 1);
@@ -24,30 +38,52 @@ xq = linspace(1.3, 9);
 
 # 1/Vm = 0.19022 -> Vm = 1/0.19022 -> Vm = 5.2571
 # Ks/Vm = 16.40224 -> Ks = 86.228
+disp(s_Linearization_First_Res);
 yq = ((5.2571 * xq)./(86.228 + xq));
+disp("\n")
+input(s_Enter_Continue);
+clc;
 
 # Caso 2: Linearizando como 1/v = Ks²/Vm * 1/s² + 1/Vm
 # temos a = Ks²/Vm, x = 1/s², b = 1/Vm
+
+disp(s_Linearization_Second_Exp);
+disp("\n")
+input(s_Enter_Continue);
+clc;
 
 # Linearizados
 x2 = 1./(s.^2);
 
 # Polinomio linearizado
+disp(s_Linearization_Poly);
 lin2 = polyfit(x2, z, 1);
+polLin2 = polyout(lin2);
+printf("P(s) = %s", polLin2);
+disp("\n");
+input(s_Enter_Continue);
+clc;
 
 # 1/Vm = 2.449 -> Vm = 1/2.449 -> Vm = 0.40833
 # Ks²/Vm = 19.3760 -> Ks² = 7.9118
+disp(s_Linearization_Second_Res);
 yq2 = (0.40833*xq.^2) ./ (7.9118 + xq.^2);
+disp("\n");
+input(s_Enter_Continue);
+clc;
 
 # Plot
 
+disp(s_Graph_Lin_Aprox_Exp);
+
 linAprox = polyval(lin, xi);
-plot(x, z, "*", xi, linAprox, ";Aproximação Linear;")
+plot(x, z, "*", xi, linAprox, ";Linearização;")
 
-input ("aperta ai");
-
-title("Aproximação por Michaelis Menten");
+input (s_Enter_Continue);
+clc;
+close;
+disp(s_Graph_MM_Aprox_Exp);
 plot (xq, yq, ";Caso 1;", xq, yq2, ";Caso 2;", s, v, "*")
-
-# p(s) = (5.2571*S)/(3.1200 + s)
-
+input(s_Enter_Continue);
+close;
+clc;
--- scripts/strings/strings_q4.m
+++ scripts/strings/strings_q4.m
@@ -1,1 +1,42 @@
 s_Adjust_Bacteria_FName = "ajuste_bacteria.txt";
+
+s_Lin_First_Case_Exp = "Para o primeiro caso do modelo de Michaelis-Menten, \
+temos a função \n\n        Vm * [S]\n v = ------------\n     Ks + [S]²\n\n Podemos \
+linearizar essa função fazendo \n\n1/v = K";
+
+s_Linearization_First_Exp = "Para o primeiro caso do modelo de Michaelis-\
+Menten, temos a função \n\nv = (Vm * S) / (Ks + S²)\n\nPodemos linearizar \
+esse modelo fazendo:\n\n1/v = (Ks + S²) / (Vm * S) -> 1/v = (Ks / Vm) * (1 \
+/ S) + (1 / Vm)\n\nPodemos linearizar usando a = (Vm * S), x = (1 / S) e b = \
+(1 / Vm), sendo essa linearização um sistema do tipo Ax = b.";
+
+s_Linearization_Poly = "Realizando o procedimento descrito \
+anteriormente e aplicando os valores dados, temos como resultado a seguinte \
+aproximação linear para o polinômio: ";
+
+s_Linearization_First_Res = "Analizando os resultados do polinômio  \
+linearizado, temos as seguintes equações: \n\n1 / Vm = 0.19022 -> Vm = \
+5.2571\n\nKs / Vm = 16.40224 -> Ks = 86.228\n\nTendo isso em mente, chegamos \
+à seguinte expressão para o primeiro caso: \n\nY(s) = ((5.2571 * s) / \
+(86.228 + s))";
+
+s_Linearization_Second_Res = "Analisando os resultados do polinômio \
+linearizado, temos as seguintes equações: \n\n1 / Vm  = 2.449 -> Vm = \
+0.40833\n\nKs² / Vm = 19.3760 -> Ks² = 7.9118\n\nConsiderando essas \
+equações, chegamos à seguinte expressão: \n\nY(s) = (0.40833 * s²) / \
+(7.9118 + s²)";
+
+s_Linearization_Second_Exp = "Para o segundo caso do modelo de Michaelis-\
+Menten, temos a função \n\nv = (Vm * S²) / (Ks² + S²)\n\nPodemos linearizar \
+esse modelo fazendo:\n\n1/v = (Ks² / Vm) / (Vm * S²) -> 1/v = (Ks² / Vm) * \
+(1 / S²) + (1 / Vm)\n\nPodemos linearizar usando a = (Ks² / Vm), x = \
+(1 / s²) e b = (1 / Vm)";
+
+s_Graph_Lin_Aprox_Exp = "O gráfico apresenta a linearização realizada para \
+possibilitar uma resposta pelo método dos quadrados mínimos.";
+
+s_Graph_MM_Aprox_Exp = "O gráfico apresenta a aproximação realizada pelas \
+equações propostas por Michaelis-Menten para o ajuste à população de \
+bactérias. Apenas analisando visualmente o gráfico já é possível perceber \
+que o segundo caso de Michaelis-Menten se adequa melhor ao crescimento \
+populacional dessa bactéria.";
